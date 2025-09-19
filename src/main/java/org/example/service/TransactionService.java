f1d0a9e2b40331ec0b126fca520719f806132a65
kaoutarlabjioui <kalabjioui@gmail.com>
2025-09-19 21:59:52 +0100

    finalisation d'historique des transactions

--- src/main/java/org/example/service/TransactionService.java
+++ src/main/java/org/example/service/TransactionService.java
@@ -8,12 +8,13 @@ import org.example.utils.AmountUtils;
 import org.example.utils.ConsoleUtils;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.example.model.Transaction.Type.DEPOSIT;
 
 public class TransactionService {
 
-    public TransactionRepository transactionRepository;
+    public static TransactionRepository transactionRepository;
     public AccountRepository accountRepository;
     public TransactionService(TransactionRepository transactionRepository , AccountRepository accountRepository) {
         this.transactionRepository = transactionRepository;
@@ -63,9 +64,82 @@ public class TransactionService {
 
         }
 
-        Account account2 = AccountService.getAccount();
+        Account account2 = null;
+        String outerAccountRib = null;
+        if (isInner){
+            account2 = AccountService.getAccount();
+            if(account1 == account2){
+                System.out.println("Cannot transfer to the same account");
+                return;
+            }
+        }else {
+            outerAccountRib = ConsoleUtils.readString("Enter outer account Rib : ");
+        }
+        BigDecimal amount = ConsoleUtils.readPositiveBigDecimal("Transfer amount: ");
+        if(!AmountUtils.hasSufficientBalance(account1.getBalance(),amount)){
+            System.out.println("insufficient balance");
+            return;
+        }
+        String description = ConsoleUtils.readString("Enter description : ");
 
+        if(isInner){
+            account1.transfer(amount,account2);
+            System.out.println("Transfer successful. New Balance : "+ account1.getBalance());
+        }else {
+            account1.transfer(amount, outerAccountRib);
+            System.out.println("Transfer successful. New Balance : "+ account1.getBalance());
+        }
 
+        Transaction transaction1;
+        Transaction transaction2 = null;
+
+        if (isInner){
+
+            transaction1 = new Transaction(
+                    account1.getAccountId(),
+                    Transaction.Type.TRANSFEROUT,
+                    amount,
+                    account2.getAccountId(),
+                    description
+            );
+            transaction2 = new Transaction(
+                    account2.getAccountId(),
+                    Transaction.Type.TRANSFERIN,
+                    amount,
+                    account1.getAccountId(),
+                    description
+            );
+
+        }else{
+            transaction1 = new Transaction(
+                    account1.getAccountId(),
+                    Transaction.Type.TRANSFEROUT,
+                    amount,
+                    outerAccountRib,
+                    description
+            );
+        }
+
+        transactionRepository.save(transaction1);
+        if(isInner){
+            transactionRepository.save(transaction2);
+        }
+
+    }
+
+    public static void listTransactions(){
+        Account account = AccountService.getAccount();
+        if (account == null) {
+            return;
+        }
+        else{
+            System.out.println("Transactions for account "+account.getAccountId()+"|"+account.getType());
+            System.out.println("---------------------------------------");
+            List<Transaction> transactions=transactionRepository.findByAccountId(account.getAccountId());
+            for (Transaction transaction : transactions) {
+                System.out.println(transaction.toString());
+            }
+        }
     }
 
 
